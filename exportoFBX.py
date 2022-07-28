e8fbdab596a540ddc07147752d54188920ec035d
congobato <cong.obato@gmail.com>
2022-07-28 14:43:34 +0700

    Update exportoFBX.py
    Update and add the new class FBXUtils that is a fbx file setting

--- exportoFBX.py
+++ exportoFBX.py
@@ -1,8 +1,3 @@
-#install and run in maya
-from importlib import rerload
-import exportFBX
-reload(exportFBX)
-
 #start code
 import pymel.core as pm
 import maya.cmds as cmds
@@ -10,12 +5,9 @@ import maya.mel as mel
 
 #rename scene
 filename = cmds.file(q=True, o=True, sm=True,w=True)
-#newname = filename.replace('.ma','_temp.ma')
-#cmds.file(rename = newname)
-#cmds.file(force=True,type='mayaAscii', s=True)
 
-#select root_joint to export FBX
-root_join = cmds.select(cmds.ls(dag=1, sl=1, type='joint'))
+#Querry file if it is not .ma
+assert filename.endswith(".ma"), "Tool only support .ma files"
 
 #delete all namespace in scene
 pm.namespaceInfo(currentNamespace = True)
@@ -25,17 +17,43 @@ for ns in sorted_ns_namespace:
   pm.namespace(removeNamespace=ns, mergeNamespaceWithParent=True)
 
 #bake key animation
-minTime = cmds.playbackOptions(q=True,minTime=True)
-maxTime = cmds.playbackOptions(q=True,maxTime=True)
-cmds.bakeResults(simulation=True, t=(minTime, maxTime), sampleBy=1)
+start = pm.playbackOptions(min=1, query=1)
+end= pm.playbackOptions(max=1, query=1)
+step= pm.playbackOptions(by=True, query=True)
 
-#export fbx file
-cmds.FBXProperties('FBXExportBakeComplexAnimation -v 1, FBXExportBakeComplexStart -q, FBXExportBakeComplexEnd -q, FBXExportInAscii y, FBXExportFileVersion -v FBX2020, FBXExportConvertUnitString -v cm')
-#mel.eval(('FBXExport -f \"{}" -s').format(filename))
+class FBXUtils:
+     """
+     a few fbx utilities
+     """
+     @classmethod
+     def fbxExport(cls, filename, start, end, step, selection=True):
+         assert isinstance(filename, str), "file_path argument must be a basestring"
+         assert type(start) is float, "start argument must be a interger"
+         assert type(end) is float, "endargument must be a interger"
+         assert type(step) is float, "step argument must be a interger"
+         assert start <= end, "start value must be smaller then end vaule"
 
-#rename fbx file
-#filename = cmds.file(q=True, o=True, sn=True)
-#fbxnames = filename.replace('.ma', '.fbx')
-#cmds.file(rename = fbxnames)
-cmds.file(force=True, type='MayaBinary', s=True)
-cmds.file(force=True, options='0', type='FBXexport', pr=True, es=True)
+         import maya.cmds as cmds
+         import maya.mel as mel
+
+         mel.eval("FBXExportBakeComplexAnimation -v true;")
+         mel.eval("FBXExportBakeComplexStart -v {start}".format(start=start))
+         mel.eval("FBXExportBakeComplexEnd -v {end}".format(end=end))
+         mel.eval("FBXExportBakeBakeResampleAnimation -v false;")
+         mel.eval("FBXExportInAscii -v 0;")
+         mel.eval("FBXExportUpAxis y")
+         mel.eval("FBXExportFileVersion FBX2020")
+         mel.eval("FBXExportConvertUnitString -v cm")
+
+         animation_step = int(step)
+         mel.eval("FBXExportBakeComplexStep -v {step}".format(step=animation_step))
+
+         #FBX!
+         mel_command = ("FBXExport -f \"" + filename + "\"").replace('.ma','.fbx')
+         if selection:
+            mel_command += "-s"
+         mel.eval(mel_command)
+         #cmds.FBXExport('-file', filename, '-s')
+         print("MEL fbx export command ->" + mel_command)
+
+FBXUtils.fbxExport(filename = filename, start=start, end=end, step=step)
